85ce5b5e32cf245b83ba9d23c1a64c84af95a7cd
cyper <345343747@qq.com>
2016-06-26 21:57:50 +0800

    update package.json

---
+++ README.md
@@ -0,0 +1,1 @@
+This plugin will try to convert line breaks inside your paragraph(p tag) into html <br> tags.
--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 module.exports = {
   // Map of hooks
   hooks: {
+    // For all the hooks, this represent the current generator
+
+    // This is called before the book is generated
+    "init": function() {
+      console.log("init!");
+    },
+
+    // This is called after the book generation
+    "finish": function() {
+      console.log("finish!");
+    },
 
     "page:before": function(page) {
+      console.log("page:before!");
       page.content = "# Title\n" +page.content;
       return page;
     },
 
     "page": function(page) {
+      console.log("page");
       page.sections[0].content = page.sections[0].content.replace("<b>", "<strong>")
       .replace("</b>", "</strong>");
       return page;
